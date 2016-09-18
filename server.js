d7ee1ac8ff4535ddc81f2eb80a10e510a02107a8
vallisarma <vedulavallisarma@gmail.com>
2016-09-18 16:04:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('http://vallisarma.imad.hasura-app.io/article-one', function (req, res) {
+app.get('article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
