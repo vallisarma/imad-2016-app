ff3852f70003b782451b1f9bbf90f32d8edad531
vallisarma <vedulavallisarma@gmail.com>
2016-09-18 16:12:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.get('/article-one', function (req, res)
   res.send('Valli - Article one');
 });
 
-app.get('/article-one', function (req, res) 
+app.get('/article-two', function (req, res) 
 {
   res.send('Valli - Article Two');
 });
 
-app.get('/article-two', function (req, res) 
+app.get('/article-three', function (req, res) 
 {
   res.send('Valli - Article Three');
 });
